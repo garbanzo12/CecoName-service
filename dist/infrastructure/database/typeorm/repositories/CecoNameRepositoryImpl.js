7581cdbdb90e43c489037f4fc6e43e8bfc942d3d
Santiago Sanchez Hernandez <122324891+garbanzo12@users.noreply.github.com>
2026-02-06 08:52:34 -0500

    Cambio en repository
    Cambio en repository para mas seguridad

--- dist/infrastructure/database/typeorm/repositories/CecoNameRepositoryImpl.js
+++ dist/infrastructure/database/typeorm/repositories/CecoNameRepositoryImpl.js
@@ -20,7 +20,7 @@ export class CecoNameRepositoryImpl {
     }));
   }
   async findById(id) {
-    const result = await this.pool.request().query(`SELECT Id,Cecocode, Name, State,CreatedBy,CreatedAt,UpdatedBy,UpdateAt FROM dbo.CecoName WHERE Id = ${id}`);
+    const result = await this.pool.request().input("id", id).query(`SELECT Id,Cecocode, Name, State,CreatedBy,CreatedAt,UpdatedBy,UpdateAt FROM dbo.CecoName WHERE Id = @id`);
     if (result.recordset.length === 0) return null; //Validación de existencia del registro 
     const row = result.recordset[0];
     return new CecoName({
--- src/infrastructure/database/typeorm/repositories/CecoNameRepositoryImpl.js
+++ src/infrastructure/database/typeorm/repositories/CecoNameRepositoryImpl.js
@@ -24,7 +24,7 @@ export class CecoNameRepositoryImpl {
 
     }
     async findById(id) {
-        const result = await this.pool.request().query(`SELECT Id,Cecocode, Name, State,CreatedBy,CreatedAt,UpdatedBy,UpdateAt FROM dbo.CecoName WHERE Id = ${id}`);
+        const result = await this.pool.request().input("id", id).query(`SELECT Id,Cecocode, Name, State,CreatedBy,CreatedAt,UpdatedBy,UpdateAt FROM dbo.CecoName WHERE Id = @id`);
         if (result.recordset.length === 0) return null //Validación de existencia del registro 
         const row = result.recordset[0];
         return new CecoName({Id: row.Id,Cecocode: row.Cecocode, Name: row.Name, State: row.State,CreatedBy: row.CreatedBy,CreatedAt: row.CreatedAt,UpdatedBy: row.UpdatedBy,UpdateAt: row.UpdateAt})
